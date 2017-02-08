d85d85fc93fc924b9e2b518b3a204746f3dc8b12
viperscape <chrisfgill@gmail.com>
2017-02-08 09:53:43 -0500

    fix watcher

--- pan/src/lifecycle.rs
+++ pan/src/lifecycle.rs
@@ -17,16 +17,16 @@ pub fn watcher (matches: &getopts::Matches) {
         let (tx, rx) = channel();
         let mut w = self::notify::watcher(tx,Duration::from_secs(5)).expect("unable to create filesys watcher");
 
-        w.watch(stratis_path.clone(),RecursiveMode::NonRecursive).expect("unable to watch directory");
+        w.watch(stratis_path.clone()+"\\stratis.exe",RecursiveMode::NonRecursive).expect("unable to watch directory");
 
-        for n in rx.recv() {
+        for n in rx.iter() {
+            println!("event: {:?}",n);
             match n {
                 DebouncedEvent::Write(path) => {
-                    //FIXME: this simply crashes on windows
-                    let r = fs::copy(path,stratis_path.clone()+"PAN_stratis.exe");
+                    let r = fs::copy(path,stratis_path.clone()+"\\PAN_stratis.exe");
                     println!("watcher-copy:{:?}",r);
                 },
-                _ => {},
+                _ => {  },
             }
         }
 
