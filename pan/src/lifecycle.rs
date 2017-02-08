040f74c4d9fdc59f5f9a177582031ca89b44944a
viperscape <chrisfgill@gmail.com>
2017-02-08 12:28:59 -0500

    finished lifecycle management of debug process

--- pan/src/lifecycle.rs
+++ pan/src/lifecycle.rs
@@ -7,28 +7,68 @@ use std::sync::mpsc::channel;
 use std::time::Duration;
 use std::env;
 use std::fs;
+use std::thread;
+use std::process::{Command,Child};
 
 /// watches the debug build, restarts server on build
 pub fn watcher (matches: &getopts::Matches) {
     if matches.opt_present("w") && matches.opt_present("d") {
         
         let stratis_path = env::var("STRATIS_DEBUG").expect("no \'STRATIS_DEBUG\' path found in environment vars");
+        let stratis_dest = stratis_path.clone()+"\\PAN_stratis.exe";
         
         let (tx, rx) = channel();
         let mut w = self::notify::watcher(tx,Duration::from_secs(5)).expect("unable to create filesys watcher");
 
         w.watch(stratis_path.clone()+"\\stratis.exe",RecursiveMode::NonRecursive).expect("unable to watch directory");
 
+        let mut spawn_handle: Option<Child> = None;
+        
         for n in rx.iter() {
+            let mut new_spawn_handle = None;
             println!("event: {:?}",n);
+            
             match n {
                 DebouncedEvent::Write(path) => {
-                    let r = fs::copy(path,stratis_path.clone()+"\\PAN_stratis.exe");
-                    println!("watcher-copy:{:?}",r);
+                    if let Some(ref mut h) = spawn_handle {
+                        if let Ok(_) = h.kill() {
+                            println!("process-killed\n");
+                            thread::sleep(Duration::from_secs(3));
+                            
+                            if let Ok(r) = fs::copy(path,&stratis_dest) {
+                                println!("watcher-copy:{:?}",r);
+                                new_spawn_handle = spawn(&stratis_dest);
+                            }
+                        }
+                    }
+                    else { //no child process alive?
+                        if let Ok(r) = fs::copy(path,&stratis_dest) {
+                            println!("watcher-copy:{:?}",r);
+                            new_spawn_handle = spawn(&stratis_dest);
+                        }
+                    }
+
+                    spawn_handle = new_spawn_handle;
+                    if let Some(ref h) = spawn_handle {
+                        println!("process-spawned:{:?}\n",h.id());
+                    }
                 },
                 _ => {  },
             }
         }
 
+    }
+}
+
+fn spawn(path: &str) -> Option<Child> {
+    let r = Command::new(path).spawn();
+    match r {
+        Ok(h) => {
+            Some(h)
+        },
+        Err(e) => {
+            println!("spawn-err:{:?}",e);
+            None
+        }
     }
 }
