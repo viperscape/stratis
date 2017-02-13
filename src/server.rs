c17d4eb556a5548a22102dac5a6a1634f9d38307
viperscape <chrisfgill@gmail.com>
2017-02-13 14:54:19 -0500

    refactor into multiple mutexes

--- src/server.rs
+++ src/server.rs
@@ -23,11 +23,12 @@ use distributor::Kind as DistKind;
 use store::{DataStore,Store};
 use player::Player;
 
+#[derive(Debug,Clone)]
 pub struct Server {
-    clients: Vec<ClientBase>, // this always grows
-    players: HashMap<Uuid, Player>,
+    clients: Arc<Mutex<Vec<ClientBase>>>, // this always grows
+    players: Arc<Mutex<HashMap<Uuid, Player>>>, //NOTE: this may make more sense as atomic-vec-keys
     dist: Sender<DistKind<TcpStream>>,
-    store: Store,
+    store: Arc<Mutex<Store>>, //NOTE: this may become a threadpool
 }
 
 impl Server {
@@ -37,15 +38,18 @@ impl Server {
         let (dist_tx, mut distributor) = Distributor::new(Store::default());
         thread::spawn(move || distributor.run());
         
-        let mut server = Server {
-            clients: vec!(),
-            players: HashMap::new(),
+        let server = Server {
+            clients: Arc::new(Mutex::new(vec!())),
+            players: Arc::new(Mutex::new(HashMap::new())),
             dist: dist_tx,
-            store: Store::default(),
+            store: Arc::new(Mutex::new(Store::default())),
         };
 
-        server.clients = server.store.clients_get();
-        let server = Arc::new(Mutex::new(server)); //FIXME: mutex glom needs to go
+        if let Ok(mut clients) = server.clients.lock() {
+            if let Ok(store) = server.store.lock() {
+                *clients = store.clients_get();
+            }
+        }
         
         for s in listener.incoming() {
             match s {
@@ -59,7 +63,7 @@ impl Server {
     }
 
     #[allow(unused_must_use)]
-    fn handler (mut server: Arc<Mutex<Server>>, mut s: TcpStream) {
+    fn handler (mut server: Server, mut s: TcpStream) {
         let mut cmd = [0;1];
                 
         //new conn needs auth code
@@ -101,33 +105,40 @@ impl Server {
         println!("client dropped");
     }
 
-    fn register (server: &mut Arc<Mutex<Server>>,
+    fn register (server: &mut Server,
                  mut s: &mut TcpStream,) {
         if let Some(c) = Client::load(&mut s) {
-            let mut server = server.lock().unwrap();
-            
-            for n in server.clients.iter() {
-                if &n.id == c.id() { continue }
+
+            if let Ok(clients) = server.clients.lock() {
+                for n in clients.iter() {
+                    if &n.id == c.id() { panic!("client already register {:?}",c.id()) }
+                }
             }
 
-            let mut r = false;
-            for _ in [..100].iter() { // try to repeatedly add a unique name
-                let mut nick = "player_".to_string();
-                nick.push_str(&rand::random::<u16>().to_string());
-                r = server.store.player_put(&c.id(), &Player { nick: nick });
-                if r { break }
-            }
+            if let Ok(store) = server.store.lock() {
+                let mut r = false;
+                for _ in [..100].iter() { // try to repeatedly add a unique name
+                    let mut nick = "player_".to_string();
+                    nick.push_str(&rand::random::<u16>().to_string());
+                    r = store.player_put(&c.id(), &Player { nick: nick });
+                    if r { break }
+                }
 
-            if r {
-                let r = server.store.client_put(&c.base);
-                println!("registered ({:?}):{:?}",r, c.id());
-                server.clients.push(c.base);
+                if r {
+                    let r = store.client_put(&c.base);
+                    println!("registered ({:?}):{:?}",r, c.id());
+
+                    if let Ok(mut clients) = server.clients.lock() {
+                        clients.push(c.base);
+                    }
+                }
+
             }
         }
     }
 
     #[allow(unused_must_use)]
-    fn chat (server: &mut Arc<Mutex<Server>>,
+    fn chat (server: &mut Server,
              mut s: &mut TcpStream,
              uuid: Uuid) {
         
@@ -137,9 +148,6 @@ impl Server {
             //broadcast
             let (mut data, bytes) = text_as_bytes(&text);
             data.extend_from_slice(bytes);
-
-            
-            let server = server.lock().unwrap();
             data.extend_from_slice(uuid.as_bytes()); //refer to uuid
             
             server.dist.send(DistKind::Broadcast(data));
@@ -147,24 +155,21 @@ impl Server {
     }
 
     #[allow(unused_must_use)]
-    fn nick (server: &mut Arc<Mutex<Server>>,
+    fn nick (server: &mut Server,
              mut s: &mut TcpStream,
              uuid: Uuid) {
         
         if let Some(text) = read_text(s) {
-            let r = {
-                let server = server.lock().unwrap();
-                server.store.player_update(&uuid, &Player { nick: text.clone() })
-            };
-
-            if r {
-                Server::send_nick(server, uuid,&text);
-                println!("nick_change:{:?}  {:?}",uuid,text);
+            if let Ok(store) = server.store.lock() {
+                if store.player_update(&uuid, &Player { nick: text.clone() }) {
+                    server.send_nick(uuid,&text);
+                    println!("nick_change:{:?}  {:?}",uuid,text);
+                }
             }
         }
     }
      #[allow(unused_must_use)]
-    fn send_nick (server: &mut Arc<Mutex<Server>>,
+    fn send_nick (&self,
                   uuid: Uuid,
                   nick: &String) {
         //broadcast
@@ -173,12 +178,11 @@ impl Server {
         data.extend_from_slice(bytes);
         data.extend_from_slice(uuid.as_bytes()); //refer to uuid
         
-        let server = server.lock().unwrap();
-        server.dist.send(DistKind::Broadcast(data));
+        self.dist.send(DistKind::Broadcast(data));
     }
 
     #[allow(unused_must_use)]
-    fn login (server: &mut Arc<Mutex<Server>>,
+    fn login (server: &mut Server,
               mut s: &mut TcpStream,
               m: Uuid) -> Option<Uuid> {
         let mut client_id = None;
@@ -187,9 +191,8 @@ impl Server {
         if let Some(c) = Client::load(&mut s) {
             let mut reg_key = None;
             
-            {
-                let mut server = server.lock().unwrap();
-                for n in server.clients.iter_mut() {
+            if let Ok(clients) = server.clients.lock() {
+                for n in clients.iter() {
                     if &n.id == c.id() {
                         reg_key = Some(n.key.clone());
                         client_id = Some(c.id().clone());
@@ -202,22 +205,24 @@ impl Server {
             if let Some(key) = reg_key {
                 let hm = hmacsha1::hmac_sha1(&key, m.as_bytes());
                 if c.key() == hm {
-                    {
-                        let mut server = server.lock().unwrap();
-                        player = server.store.player_get(c.id());
+                    
+                    if let Ok(store) = server.store.lock() {
+                        player = store.player_get(c.id());
                     }
                     
                     if let Some(player) = player {
                         if let Ok(stmp) = s.try_clone() {
                             {
-                                let mut server = server.lock().unwrap();
                                 server.dist.send(DistKind::Add(*c.id(),stmp));
-                                server.players.insert(*c.id(),player.clone());
-                                println!("total players:{:?}",server.players.len());
+
+                                if let Ok(mut players) = server.players.lock() {
+                                    players.insert(*c.id(),player.clone());
+                                    println!("total players:{:?}",players.len()); //NOTE: this should print on debug only
+                                }
                             }
 
                             println!("login:{:?}",c.id());
-                            Server::send_nick(server, *c.id(), &player.nick);
+                            server.send_nick(*c.id(), &player.nick);
                         }
                     }
                 }
@@ -235,19 +240,20 @@ impl Server {
     #[allow(unused_must_use)]
     //NOTE: this is very innefficient, and hogs the mutex
     //TODO: rework state updates entirely
-    fn send_nicks (server: &mut Arc<Mutex<Server>>,
+    fn send_nicks (server: &mut Server,
                    uuid: Uuid)  {
-        let server = server.lock().unwrap();
-        
-        for (ref pid,player) in server.players.iter() {
-            let (mut data, bytes) = text_as_bytes(&player.nick);
-            data[0] = 3; // specify nick route
-            
-            data.extend_from_slice(bytes);
-            data.extend_from_slice(pid.as_bytes()); //refer to uuid
-            
-            
-            server.dist.send(DistKind::Select(uuid,data));
+
+        if let Ok(players) = server.players.lock() {
+            for (ref pid,player) in players.iter() {
+                let (mut data, bytes) = text_as_bytes(&player.nick);
+                data[0] = 3; // specify nick route
+                
+                data.extend_from_slice(bytes);
+                data.extend_from_slice(pid.as_bytes()); //refer to uuid
+                
+                server.dist.send(DistKind::Select(uuid,data));
+            }
+
         }
     }
 }
