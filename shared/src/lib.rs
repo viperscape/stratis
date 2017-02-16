08ae2c133a26eaea0eb824052901693593e773eb
viperscape <chrisfgill@gmail.com>
2017-02-16 16:31:26 -0500

    building out ffi for c#

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,4 +1,8 @@
+extern crate uuid;
+
 pub mod client;
 pub mod chat;
 pub mod player;
 pub mod opcode;
+
+pub use uuid::Uuid;
--- unity/src/lib.rs
+++ unity/src/lib.rs
@@ -1,3 +1,47 @@
 extern crate stratis_shared as shared;
 
+use std::mem::transmute;
+
 use shared::client::Client;
+use shared::Uuid;
+
+#[no_mangle]
+pub extern fn new_client() -> *mut Client {
+    unsafe { transmute(Box::new(Client::new())) }
+}
+
+#[no_mangle]
+pub extern fn default_client(key: [u8;20], uuid: [u8;16]) -> *mut Client {
+    if let Ok(id) = Uuid::from_bytes(&uuid) {
+        unsafe { transmute(Box::new(Client::default(key, id))) }
+    }
+    else { new_client() }
+}
+
+#[no_mangle]
+pub extern fn drop_client(cptr: *mut Client) {
+    let _: Box<Client> = unsafe { transmute(cptr) };
+}
+
+//TODO: figure out refs, or just use a cstruct perhaps?
+#[no_mangle]
+pub extern fn get_client_id(cptr: *mut Client) -> [u8;16] {
+    let client = unsafe { & *cptr };
+    let mut id = [0u8;16];
+    for (i,n) in client.base.id.as_bytes().iter().enumerate() {
+        id[i] = *n;
+    }
+
+    id
+}
+
+#[no_mangle]
+pub extern fn get_client_key(cptr: *mut Client) -> [u8;20] {
+    let client = unsafe { & *cptr };
+    let mut key = [0u8;20];
+    for (i,n) in client.base.key.iter().enumerate() {
+        key[i] = *n;
+    }
+
+    key
+}
